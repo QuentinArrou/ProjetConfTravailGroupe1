d61561b7a0e3e1f10ce8a4182c1720ed6711b5d5
Quentin Arrou <quentin.arrou@univ-tlse3.fr>
2024-10-29 15:28:17 +0100

    Implémentation de la version supplémentaire numéro 1

--- Extracteur_Meteo.sh
+++ Extracteur_Meteo.sh
@@ -22,17 +22,22 @@ fi
 # 2. Extraire du fichier la température actuelle et la prévision pour le lendemain
 TEMP_ACTUELLE=$(grep -o '"temp_C":[^,]*' "meteo_$VILLE.txt" | head -n 1 | sed 's/[^0-9\-]*//g')
 TEMP_PREVISION=$(grep -o '"avgtempC":[^,]*' "meteo_$VILLE.txt" | sed -n '2p' | sed 's/[^0-9\-]*//g')
+VITESSE_VENT=$(grep -o '"windspeedKmph":[^,]*' "meteo_$VILLE.txt" | head -n 1 | sed 's/[^0-9]*//g')
+TAUX_HUMIDITE=$(grep -o '"humidity":[^,]*' "meteo_$VILLE.txt" | head -n 1 | sed 's/[^0-9]*//g')
+VISIBILITE=$(grep -o '"visibility":[^,]*' "meteo_$VILLE.txt" |head -n 1 | sed 's/[^0-9]]*//g')
 
 # 3. Affichage et formatage des températures récupérées
 echo "Température actuelle : ${TEMP_ACTUELLE}°C"
+echo "Vitesse du vent : ${VITESSE_VENT} km/h"
+echo "Taux d'humidité : ${TAUX_HUMIDITE}%"
+echo "Visibilité : ${VISIBILITE} km"
 echo "Température prévue demain : ${TEMP_PREVISION}°C"
-
 
 # 4. Enregistrer température actuelle et prévision du lendemain dans fichier meteo.txt
 DATE=$(date +"%Y-%m-%d")
 HEURE=$(date +"%H:%M")
-echo "$DATE - $HEURE - Ville : $VILLE - Température actuelle : ${TEMP_ACTUELLE}°C - Prévision du lendemain : ${TEMP_PREVISION}°C" >> meteo.txt
+echo "$DATE - $HEURE - Ville : $VILLE - Température actuelle : ${TEMP_ACTUELLE}°C - Vitesse du vent : ${VITESSE_VENT} km/h - Taux d'humidité : ${TAUX_HUMIDITE}% - Visibilité : ${VISIBILITE} km - Prévision du lendemain : ${TEMP_PREVISION}°C" >> meteo.txt
 rm "meteo_$VILLE.txt"
 
 # 5. Enregistrer en plus tout  ces informations dans des logs journaliers
-echo "$DATE - $HEURE - Ville : $VILLE - Température actuelle : ${TEMP_ACTUELLE}°C - Prévision du lendemain : ${TEMP_PREVISION}°C" >> "meteo_$DATE.txt"
+echo "$DATE - $HEURE - Ville : $VILLE - Température actuelle : ${TEMP_ACTUELLE}°C - Vitesse du vent : ${VITESSE_VENT} km/h - Taux d'humidité : ${TAUX_HUMIDITE}% - Visibilité : ${VISIBILITE} km -Prévision du lendemain : ${TEMP_PREVISION}°C" >> "meteo_$DATE.txt"
